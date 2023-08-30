e62a3128d0e9f97ad7c3260a86b072e6d45ab610
marcosgrac <mgs8cin@gmail.com>
2023-08-29 21:23:08 -0300

    Primeiro commit

---
+++ src/test/java/DesafioNEXT2023/Seguro/SeguroApplicationTests.java
@@ -0,0 +1,13 @@
+package DesafioNEXT2023.Seguro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
